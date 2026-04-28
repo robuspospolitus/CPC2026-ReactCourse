1c425ee165d683466120f826d0159bed6c256350
Nadia Gill <nadia.gill2006@gmail.com>
2026-04-28 12:27:24 +0200

    step 5: make a new expense

--- cpc2026-react-javastript/src/App.jsx
+++ cpc2026-react-javastript/src/App.jsx
@@ -5,10 +5,24 @@ function App() {
   const [amount, setAmount] = useState('');
 
   console.log(title + " " + amount);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const newExpense = {
+      id: Math.random().toString(),
+      title: title,
+      amount: +amount,
+    };
+    
+    // Resetowanie pól
+    setTitle('');
+    setAmount('');
+  };
+
   return (
     <div className="container">
       <h1>Menedżer wydatków</h1>
-      <form>
+      <form onSubmit={(event) => submitHandler(event)}>
         <input type="text" placeholder="Nazwa wydatku" value={title} onChange={(e) => setTitle(e.target.value)}/>
         <input type="number" placeholder="Kwota" value={amount} onChange={(e) => setTitle(e.target.value)}/>
         <button type="submit">Dodaj wydatek</button>
