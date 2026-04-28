9ee910b515c57b6ca0186a85848b60e77cc1c2c7
Nadia Gill <nadia.gill2006@gmail.com>
2026-04-28 12:14:40 +0200

    step 4: saved form data

--- cpc2026-react-javastript/src/App.jsx
+++ cpc2026-react-javastript/src/App.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
+
 function App() {
-  
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState('');
+
+  console.log(title + " " + amount);
   return (
     <div className="container">
       <h1>Menedżer wydatków</h1>
       <form>
-        <input type="text" placeholder="Nazwa wydatku"/>
-        <input type="number" placeholder="Kwota"/>
+        <input type="text" placeholder="Nazwa wydatku" value={title} onChange={(e) => setTitle(e.target.value)}/>
+        <input type="number" placeholder="Kwota" value={amount} onChange={(e) => setTitle(e.target.value)}/>
         <button type="submit">Dodaj wydatek</button>
       </form>
     </div>
