6041c80cd6396bc64de75ecfd289c87dbf77fb93
Nadia Gill <nadia.gill2006@gmail.com>
2026-04-28 22:07:17 +0200

    step 14: routing with stats

---
+++ cpc2026-react-javastript/src/components/Stats.jsx
@@ -0,0 +1,15 @@
+import { useContext } from 'react';
+import { ExpenseContext } from '../context/ExpenseContext';
+
+export default function Stats() {
+    const { expenses } = useContext(ExpenseContext);
+    const total = expenses.reduce((sum, exp) => sum + Number(exp.amount), 0);
+
+    return (
+        <div className="container stats-page">
+            <h1>Podsumowanie finansowe</h1>
+            <p className="total-badge">{total.toFixed(2)} PLN</p>
+            <p>Liczba transakcji: {expenses.length}</p>
+        </div>
+    )
+}
--- cpc2026-react-javastript/src/main.jsx
+++ cpc2026-react-javastript/src/main.jsx
@@ -3,11 +3,28 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles/styles.css';
 import { ExpenseProvider } from './context/ExpenseContext.jsx';
+import { BrowserRouter, Routes, Route, Link } from 'react-router';
+import Stats from './components/Stats.jsx';
+import Logo from './assets/logo.png';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ExpenseProvider>
-      <App />
+      <BrowserRouter>
+        <nav className='navbar'>
+          <div className="nav-logo">
+            <img height={64} src={Logo} alt="" />
+          </div>
+          <div className="nav-links">
+            <Link to="/">Portfel</Link>
+            <Link to="/stats">Statystyki</Link>
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/stats" element={<Stats />} />
+        </Routes>
+      </BrowserRouter>
     </ExpenseProvider>
   </StrictMode>,
 )
